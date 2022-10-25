cb9a7d89934b0fec4fea4ac3efdeb876ddff9f3a
Michael Okoe <michael.okoejr@gmail.com>
2022-10-25 02:59:57 +0000

    Work-in-progress

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,4 @@
 class Author < ApplicationRecord
+  validates :name, presense: uniqueness:  true
+  validates :phone_number, length: { maximum: 10 }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+  VALID_ATTRIBUTES = ("Fiction", "Non-Fiction")
+  validates :title, presense: true
+  validates :content, length: { minimum 250 }
+  validates :summary, lenght: { maximum 250 }
+  validates_inclusion_of :category, in => VALID_ATTRIBUTES
+  validate :must_contain_wont_believe, :must_contain_secret, 
+           :must_contain_top_number, :must_contain_guess
+  
+  def must_contain_wont_believe
+    if title.present?
+  end
 end
