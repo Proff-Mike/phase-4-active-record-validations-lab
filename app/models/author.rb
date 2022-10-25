3857e1868e24af87b0edb7ebed7e7cd354059582
Michael Okoe <michael.okoejr@gmail.com>
2022-10-25 01:39:18 +0000

    commit tree c9dc15632cd4c53321ba57fb8dac128f88ce7221

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ApplicationRecord
+  validates :name, presense: true
+  validates :name, uniqueness: true
+  validates :phone_number, length: { maximum: 10 }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ApplicationRecord
+  validates :title, presense: true
+  validates :content, length: { minimum 250 }
+  validates :summary, lenght: { maximum 250 }
+  validate :must_contain_wont_believe, :must_contain_secret, 
+           :must_contain_top_number, :must_contain_guess
+  
+  def must_contain_wont_believe
+    if title.present?
+  end
 end
